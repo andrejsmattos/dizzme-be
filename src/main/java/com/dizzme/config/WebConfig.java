9baf334727a5eac117f94e531a27dca14501e0f8
André <andrejusmattos@gmail.com>
2025-10-03 20:56:20 -0300

    Liberação explícita de mapeamento /api/**

--- src/main/java/com/dizzme/config/WebConfig.java
+++ src/main/java/com/dizzme/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOriginPatterns(
                         "http://localhost:*",             // dev local
                         "https://localhost:*",            // dev https local
